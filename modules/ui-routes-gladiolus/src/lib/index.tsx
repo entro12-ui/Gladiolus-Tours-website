ae63bf0df418accd6d55393d234784678abd46ac
colspan <collin@collindigm.com>
2023-11-22 11:41:43 +0300

    Gladiolus Routes Updated

--- modules/ui-routes-gladiolus/src/lib/index.tsx
+++ modules/ui-routes-gladiolus/src/lib/index.tsx
@@ -1,7 +1,6 @@
-import {generatePath, Navigate} from "react-router-dom";
-import {GtToursRoute} from "./route-parameter-hooks";
+import { generatePath, Navigate } from 'react-router-dom';
+import { GtToursRoute } from './routes';
 
 export const NotFoundRedirect = () => {
   return <Navigate to={generatePath(GtToursRoute.NotFound)} />;
-}
-
+};
--- modules/ui-routes-gladiolus/src/lib/routes.ts
+++ modules/ui-routes-gladiolus/src/lib/routes.ts
@@ -1,9 +1,14 @@
+export enum GtToursRouteParam {
+  ItineraryId = 'itineraryId',
+  PackageId = 'packageId',
+  SafariId = 'safariId',
+}
 export enum GtToursRoute {
-    HomePage = "/",
-    AboutUs = "/about-us",
-    Destinations = "/destinations",
-    Safaris = "/safaris",
-    Accommodation = "/accommodation",
-    Contact = "/contact",
-    NotFound = "/404",
+  HomePage = '/',
+  AboutUs = '/about-us',
+  Destinations = '/destinations',
+  Safaris = '/safaris',
+  Accommodation = '/accommodation',
+  Contact = '/contact',
+  NotFound = '/404',
 }
