814d3f158720a14b91d6cc8eae064e2432c357ba
colspan <collin@collindigm.com>
2023-12-02 16:31:03 +0300

    Gladiolus Tour Accomodations Page Initiated

--- modules/ui-page-accomodations/src/index.ts
+++ modules/ui-page-accomodations/src/index.ts
@@ -1,1 +1,3 @@
-export * from './lib/ui-page-accomodations';
+import { GladiolusAccommodationsToursPage } from './lib/index';
+
+export default GladiolusAccommodationsToursPage;
---
+++ modules/ui-page-accomodations/src/lib/index.tsx
@@ -0,0 +1,7 @@
+export const GladiolusAccommodationsToursPage = () => {
+  return (
+    <div>
+      <h1>Gladiolus Accommodations Tours Page</h1>
+    </div>
+  );
+};
