5c80c760e8c4ed770ff454e83caf6723ea927aa2
colspan <collin@collindigm.com>
2023-12-04 00:18:09 +0300

    Gladiolus Team Component Prototype

--- modules/ui-page-home-index/src/lib/index.tsx
+++ modules/ui-page-home-index/src/lib/index.tsx
@@ -4,6 +4,7 @@ import { GladiolusStats } from './stats';
 import { GladiolusSafaris } from './safaris';
 import { GladiolusServices } from './services';
 import { GladiolusTestimonials } from './testimonials';
+import { GladiolusTeam } from './team-sections';
 
 export const GladiolusLandingPage = () => {
   return (
@@ -24,6 +25,9 @@ export const GladiolusLandingPage = () => {
         </div>
         <div>
           <GladiolusTestimonials />
+        </div>
+        <div>
+          <GladiolusTeam />
         </div>
       </main>
     </div>
