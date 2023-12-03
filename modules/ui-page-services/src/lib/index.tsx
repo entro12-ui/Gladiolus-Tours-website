70fec967c369c7c7ebc85fcaddd3a2758e355cf2
colspan <collin@collindigm.com>
2023-12-04 00:31:36 +0300

    Gladiolus Tours Service Page initiated with header section

--- modules/ui-page-services/src/lib/index.tsx
+++ modules/ui-page-services/src/lib/index.tsx
@@ -1,7 +1,14 @@
+import { EntryPageHelmet } from '@gladiolus/ui-comp-page-helmet';
+import GladiolusHeaderSection from '@gladiolus/ui-comp-header';
+
 export const GladiolusService = () => {
   return (
-    <div>
-      <h1>Gladiolus Tours Service Page</h1>
+    <div className="bg-white">
+      <EntryPageHelmet title={'Services'} />
+      <GladiolusHeaderSection
+        header={'Services'}
+        description={'Services Offered by Gladiolus Tours'}
+      />
     </div>
   );
 };
