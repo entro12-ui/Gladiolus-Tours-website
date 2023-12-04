da584aa19e19b91d7c8fae0a1b078bbac8430291
colspan <collin@collindigm.com>
2023-12-04 08:40:37 +0300

    Navigation bar update with a language drop down

--- modules/ui-layout-gladiolus/src/lib/navbar.tsx
+++ modules/ui-layout-gladiolus/src/lib/navbar.tsx
@@ -1,7 +1,8 @@
-import { Dialog } from '@headlessui/react';
+import { Dialog, Popover, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { GtToursRoute } from '@gladiolus/ui-routes-gladiolus';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
 const navigation = [
   { name: 'Home', href: GtToursRoute.Home },
@@ -11,6 +12,33 @@ const navigation = [
   { name: 'Accommodations', href: GtToursRoute.Accommodation },
   { name: 'About Us', href: GtToursRoute.AboutUs },
   { name: 'Contact Us', href: GtToursRoute.Contact },
+];
+
+const languages = [
+  {
+    name: 'About us',
+    href: '#',
+    description:
+      'Learn more about our company values and mission to empower others',
+  },
+  {
+    name: 'Careers',
+    href: '#',
+    description:
+      'Looking for you next career opportunity? See all of our open positions',
+  },
+  {
+    name: 'Support',
+    href: '#',
+    description:
+      'Get in touch with our dedicated support team or reach out on our community forums',
+  },
+  {
+    name: 'Blog',
+    href: '#',
+    description:
+      'Read our latest announcements and get perspectives from our team',
+  },
 ];
 
 export const GladiolusHeader = () => {
@@ -50,9 +78,45 @@ export const GladiolusHeader = () => {
               {item.name}
             </a>
           ))}
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Language <span aria-hidden="true">&rarr;</span>
-          </a>
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+              Languages
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none text-gray-400"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-96 rounded-3xl bg-white p-4 shadow-lg ring-1 ring-gray-900/5">
+                {languages.map((item) => (
+                  <div
+                    key={item.name}
+                    className="relative rounded-lg p-4 hover:bg-gray-50"
+                  >
+                    <a
+                      href={item.href}
+                      className="block text-sm font-semibold leading-6 text-gray-900"
+                    >
+                      {item.name}
+                      <span className="absolute inset-0" />
+                    </a>
+                    <p className="mt-1 text-sm leading-6 text-gray-600">
+                      {item.description}
+                    </p>
+                  </div>
+                ))}
+              </Popover.Panel>
+            </Transition>
+          </Popover>
         </div>
       </nav>
       <Dialog
@@ -95,12 +159,45 @@ export const GladiolusHeader = () => {
                 ))}
               </div>
               <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Languages
-                </a>
+                <Popover className="relative">
+                  <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+                    Languages
+                    <ChevronDownIcon
+                      className="h-5 w-5 flex-none text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-96 rounded-3xl bg-white p-4 shadow-lg ring-1 ring-gray-900/5">
+                      {languages.map((item) => (
+                        <div
+                          key={item.name}
+                          className="relative rounded-lg p-4 hover:bg-gray-50"
+                        >
+                          <a
+                            href={item.href}
+                            className="block text-sm font-semibold leading-6 text-gray-900"
+                          >
+                            {item.name}
+                            <span className="absolute inset-0" />
+                          </a>
+                          <p className="mt-1 text-sm leading-6 text-gray-600">
+                            {item.description}
+                          </p>
+                        </div>
+                      ))}
+                    </Popover.Panel>
+                  </Transition>
+                </Popover>
               </div>
             </div>
           </div>
