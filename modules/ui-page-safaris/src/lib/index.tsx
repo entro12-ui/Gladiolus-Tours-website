269bf05c0e3f957713e87c619c7582391fa928f6
colspan <collin@collindigm.com>
2023-12-04 08:04:56 +0300

    Safaris Page Initialized with head and header section

--- modules/ui-page-safaris/src/lib/index.tsx
+++ modules/ui-page-safaris/src/lib/index.tsx
@@ -1,7 +1,14 @@
+import { EntryPageHelmet } from '@gladiolus/ui-comp-page-helmet';
+import GladiolusHeaderSection from '@gladiolus/ui-comp-header';
+
 export const GladiolusToursSafarisPage = () => {
   return (
-    <div>
-      <h1>Gladiolus Tours Safaris Page</h1>
+    <div className="bg-white">
+      <EntryPageHelmet title={'Safaris'} />
+      <GladiolusHeaderSection
+        header={'Safaris'}
+        description={'Safaris Offered by Gladiolus Tours'}
+      />
     </div>
   );
 };
