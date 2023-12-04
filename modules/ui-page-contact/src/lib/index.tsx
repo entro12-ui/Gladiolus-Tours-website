4d5df71318e8b50b76e6c2e5882dfc044fccae35
colspan <collin@collindigm.com>
2023-12-04 08:13:05 +0300

    Gladiolus Tours Contact Page initiated

--- modules/ui-page-contact/src/lib/index.tsx
+++ modules/ui-page-contact/src/lib/index.tsx
@@ -1,7 +1,14 @@
+import { EntryPageHelmet } from '@gladiolus/ui-comp-page-helmet';
+import GladiolusHeaderSection from '@gladiolus/ui-comp-header';
+
 export const GladiolusToursContactPage = () => {
   return (
-    <div>
-      <h1>Gladiolus Tours Contact Page</h1>
+    <div className="bg-white">
+      <EntryPageHelmet title={'Contact'} />
+      <GladiolusHeaderSection
+        header={'Contact'}
+        description={'Gladiolus Tours Contacts'}
+      />
     </div>
   );
 };
