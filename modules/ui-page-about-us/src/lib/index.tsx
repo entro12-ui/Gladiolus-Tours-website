9afadea3120fe88e8a6fe7c508b729da9119563c
colspan <collin@collindigm.com>
2023-12-05 13:56:29 +0300

    About Us Page initiated

--- modules/ui-page-about-us/src/lib/index.tsx
+++ modules/ui-page-about-us/src/lib/index.tsx
@@ -1,14 +1,20 @@
 import { EntryPageHelmet } from '@gladiolus/ui-comp-page-helmet';
 import GladiolusHeaderSection from '@gladiolus/ui-comp-header';
+import { GladiolusToursAboutPage } from './main';
 
 export const GladiolusToursAboutUs = () => {
   return (
-    <div className="bg-white">
-      <EntryPageHelmet title={'About us'} />
-      <GladiolusHeaderSection
-        header={'About us'}
-        description={'About us Offered by Gladiolus Tours'}
-      />
-    </div>
+    <>
+      <div className="bg-white">
+        <EntryPageHelmet title={'About us'} />
+        <GladiolusHeaderSection
+          header={'About us'}
+          description={'About Gladiolus Tours'}
+        />
+      </div>
+      <div>
+        <GladiolusToursAboutPage />
+      </div>
+    </>
   );
 };
