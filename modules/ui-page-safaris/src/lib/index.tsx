9ca2db95bc1a18b5a116709922f9d807ea213653
colspan <collin@collindigm.com>
2023-12-08 21:24:33 +0300

    Safaris Persistence Path updated

--- modules/ui-page-safaris/src/lib/index.tsx
+++ modules/ui-page-safaris/src/lib/index.tsx
@@ -1,23 +1,54 @@
 import { EntryPageHelmet } from '@gladiolus/ui-comp-page-helmet';
 import GladiolusHeaderSection from '@gladiolus/ui-comp-header';
-
-interface IProps {
-  id: number;
-  name: string;
-  type: string;
-  price: number;
-  status: boolean;
-  description: string;
-}
+import { Link } from 'react-router-dom';
+import { GtToursRoute } from '@gladiolus/ui-routes-gladiolus';
+import { Safaris } from '@gladiolus/ui-gladiolus-persistence';
 
 export const GladiolusToursSafarisPage = () => {
   return (
-    <div className="bg-white">
-      <EntryPageHelmet title={'Safaris'} />
-      <GladiolusHeaderSection
-        header={'Safaris'}
-        description={'Safaris Offered by Gladiolus Tours'}
-      />
-    </div>
+    <>
+      <div className="bg-white">
+        <EntryPageHelmet title={'Safaris'} />
+        <GladiolusHeaderSection
+          header={'Safaris'}
+          description={'Safaris Offered by Gladiolus Tours'}
+        />
+      </div>
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
+          {Safaris.map((safari) => (
+            <div
+              key={safari.id}
+              className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
+            >
+              <div className="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+                <img
+                  src=""
+                  alt=""
+                  className="h-full w-full object-cover object-center sm:h-full sm:w-full"
+                />
+              </div>
+              <div className="flex flex-1 flex-col space-y-2 p-4">
+                <h3 className="text-sm font-medium text-gray-900">
+                  <Link to={GtToursRoute.Safaris}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {safari.name}
+                  </Link>
+                </h3>
+                <p className="text-sm text-gray-500">{safari.description}</p>
+                <div className="flex flex-1 flex-col justify-end">
+                  <p className="text-sm italic text-gray-500">
+                    {safari.status}
+                  </p>
+                  <p className="text-base font-medium text-gray-900">
+                    {safari.price}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
