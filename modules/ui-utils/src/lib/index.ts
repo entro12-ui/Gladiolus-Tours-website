d4fa32d9fb2e5c09e8a35017b06a4874d8ec520d
colspan <collin@collindigm.com>
2023-12-10 13:04:52 +0300

    Util: Random Int Exclusive Upper Bound

--- modules/ui-utils/src/lib/index.ts
+++ modules/ui-utils/src/lib/index.ts
@@ -20,3 +20,9 @@ export function getWebsiteBaseHref() {
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
+
+export function getRandomIntExclusiveUpperBound(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
