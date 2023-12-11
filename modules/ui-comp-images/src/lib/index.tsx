8fca4c4149727f974aa6ec9dd3aef926e8df44a4
colspan <collin@collindigm.com>
2023-12-11 13:29:11 +0300

    Random Image Generate Component

--- modules/ui-comp-images/src/lib/index.tsx
+++ modules/ui-comp-images/src/lib/index.tsx
@@ -1,11 +1,38 @@
-interface IProps {
-  alt: string;
-  src: string;
+import Image0 from './images/buffalo.webp';
+import Image1 from './images/cave_lion.webp.webp';
+import Image2 from './images/buffalo_grass.webp';
+import Image3 from './images/cheetah.webp';
+import Image4 from './images/elephant.webp';
+import Image5 from './images/zebras.webp';
+import Image6 from './images/ngorongoro_zebra.webp';
+import Image7 from './images/lion.webp';
+import Image8 from './images/lioness.webp';
+import Image9 from './images/elephant_family.webp';
+import { getRandomIntExclusiveUpperBound } from '@gladiolus/ui-utils';
+
+const images = [
+  Image0,
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
+  Image8,
+  Image9,
+];
+
+interface Props {
+  className?: string;
 }
-export const GladiolusToursImage = ({ src, alt }: IProps) => {
-  return <img src={src} alt={alt} />;
+export const GladiolusToursImage = ({ className }: Props) => {
+  const choice = getRandomIntExclusiveUpperBound(0, images.length);
+  return (
+    <img src={images[choice]} className={className} alt={'Gladiolus Tours'} />
+  );
 };
 
-export const GladiolusToursLogo = ({ src, alt }: IProps) => {
-  return <img src={src} alt={alt} />;
+export const GladiolusToursLogo = () => {
+  return null;
 };
