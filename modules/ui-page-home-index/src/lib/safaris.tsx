1dd7f59fc5a8b86b2cb58f3303aeefea8d3da2b7
colspan <collin@collindigm.com>
2023-12-12 19:15:04 +0300

    Unlock the Wonders of Gladiolus Tours Safaris

--- modules/ui-page-home-index/src/lib/safaris.tsx
+++ modules/ui-page-home-index/src/lib/safaris.tsx
@@ -1,80 +1,93 @@
-import {
-  CloudArrowUpIcon,
-  LockClosedIcon,
-  ServerIcon,
-} from '@heroicons/react/20/solid';
-import { GladiolusToursImage } from '@gladiolus/ui-comp-images';
+import Visitor from './images/Visitors_And_Giraffes.webp';
+import Zanzibar from './images/Zanzibar.webp';
+import Walking from './images/Walking.webp';
+import Sunrise from './images/Sunrise.webp';
 
-const features = [
+interface IFeatures {
+  name: string;
+  description: string;
+}
+
+const features: IFeatures[] = [
   {
-    name: 'Push to deploy.',
+    name: 'Your Custom Safari Experience',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: CloudArrowUpIcon,
+      'Embark on a journey like no other with Gladiolus Tours Safaris, your trusted partner for tailor-made, unforgettable safaris in the heart of Tanzania.',
   },
   {
-    name: 'SSL certificates.',
+    name: 'Crafting Unique Adventures',
     description:
-      'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: LockClosedIcon,
+      'Indulge in experiences designed exclusively for you, tailored to your preferences and budget. Your dream adventure awaits with Gladiolus Tours Safaris.',
   },
   {
-    name: 'Database backups.',
+    name: 'Privately Yours, Authentically Wild',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ServerIcon,
+      'Step into a world of private and authentic safaris, meticulously designed for a personalized experience. Our passionate driver-guides speak multiple languages, ensuring your journey is truly exceptional.',
+  },
+  {
+    name: 'Responsive, Flexible, Unforgettable',
+    description:
+      'Our responsive team is dedicated to turning your last-minute requests into reality. Whether adapting your trip or finding the ideal solution on the go, your Gladiolus adventure is marked by responsiveness and flexibility.',
+  },
+  {
+    name: 'Discover Tanzania and Zanzibar',
+    description:
+      'Embark on tailor-made tours to Tanzania and Zanzibar. Our partners share our commitment to preserving the environment and respecting local traditions. Let our experienced guides lead you to unique experiences, from climbing Kilimanjaro to relaxing on the idyllic beaches of Zanzibar.',
+  },
+  {
+    name: 'Safe Travels, Memorable Moments',
+    description:
+      "Travel with complete peace of mind with Gladiolus Safaris, a registered company in Tanzania, fully licensed to operate safaris. Our local offices guarantee immediate assistance, ensuring you're in good hands for a safe and memorable travel experience.",
   },
 ];
 
 export const GladiolusSafaris = () => {
+  const alternativeText = 'Gladiolus Safaris';
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl md:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
-          <div className="px-6 lg:px-0 lg:pr-4 lg:pt-4">
-            <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">
-                Deploy faster
-              </h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                A better workflow
-              </p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
-              </p>
-              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                {features.map((feature) => (
-                  <div key={feature.name} className="relative pl-9">
-                    <dt className="inline font-semibold text-gray-900">
-                      <feature.icon
-                        className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
-                        aria-hidden="true"
-                      />
-                      {feature.name}
-                    </dt>{' '}
-                    <dd className="inline">{feature.description}</dd>
-                  </div>
-                ))}
-              </dl>
-            </div>
-          </div>
-          <div className="sm:px-6 lg:px-0">
-            <div className="relative isolate overflow-hidden bg-indigo-500 px-6 pt-8 sm:mx-auto sm:max-w-2xl sm:rounded-3xl sm:pl-16 sm:pr-0 sm:pt-16 lg:mx-0 lg:max-w-none">
-              <div
-                className="absolute -inset-y-px -left-3 -z-10 w-full origin-bottom-left skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white"
-                aria-hidden="true"
-              />
-              <div className="mx-auto max-w-2xl sm:mx-0 sm:max-w-none">
-                <GladiolusToursImage className="-mb-12 w-[57rem] max-w-none rounded-tl-xl bg-gray-800 ring-1 ring-white/10" />
-                <div
-                  className="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 sm:rounded-3xl"
-                  aria-hidden="true"
-                />
+    <div className="bg-white">
+      <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+        <div>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Unlock the Wonders of Gladiolus Tours Safaris
+          </h2>
+          <p className="mt-4 text-gray-500">
+            Immerse yourself in the extraordinary with Gladiolus Safaris. Our
+            bespoke safaris in Tanzania redefine adventure, offering
+            personalized experiences crafted just for you.
+          </p>
+
+          <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+            {features.map((feature) => (
+              <div key={feature.name} className="border-t border-gray-200 pt-4">
+                <dt className="font-medium text-gray-900">{feature.name}</dt>
+                <dd className="mt-2 text-sm text-gray-500">
+                  {feature.description}
+                </dd>
               </div>
-            </div>
-          </div>
+            ))}
+          </dl>
+        </div>
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+          <img
+            src={Visitor}
+            alt={alternativeText}
+            className="rounded-lg bg-gray-100"
+          />
+          <img
+            src={Sunrise}
+            alt={alternativeText}
+            className="rounded-lg bg-gray-100"
+          />
+          <img
+            src={Walking}
+            alt={alternativeText}
+            className="rounded-lg bg-gray-100"
+          />
+          <img
+            src={Zanzibar}
+            alt={alternativeText}
+            className="rounded-lg bg-gray-100"
+          />
         </div>
       </div>
     </div>
