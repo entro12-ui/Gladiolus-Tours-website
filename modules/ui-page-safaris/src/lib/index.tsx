3c2a59d6cfb9891f597acc64c8de8bb7bd2b9b97
colspan <collin@collindigm.com>
2023-12-13 22:56:16 +0300

    Gladiolus Safaris Component

--- modules/ui-page-safaris/src/lib/index.tsx
+++ modules/ui-page-safaris/src/lib/index.tsx
@@ -1,9 +1,6 @@
 import { EntryPageHelmet } from '@gladiolus/ui-comp-page-helmet';
 import GladiolusHeaderSection from '@gladiolus/ui-comp-header';
-import { Link } from 'react-router-dom';
-import { GtToursRoute } from '@gladiolus/ui-routes-gladiolus';
-import { Safaris } from '@gladiolus/ui-gladiolus-persistence';
-import { GladiolusToursImage } from '@gladiolus/ui-comp-images';
+import { GladiolusSafaris } from '@gladiolus/ui-comp-gladiolus-safaris';
 
 export const GladiolusToursSafarisPage = () => {
   return (
@@ -15,36 +12,8 @@ export const GladiolusToursSafarisPage = () => {
           description={'Safaris Offered by Gladiolus Tours'}
         />
       </div>
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-          {Safaris.map((safari) => (
-            <div
-              key={safari.id}
-              className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
-            >
-              <div className="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
-                <GladiolusToursImage className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none" />
-              </div>
-              <div className="flex flex-1 flex-col space-y-2 p-4">
-                <h3 className="text-sm font-medium text-gray-900">
-                  <Link to={GtToursRoute.Safaris}>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {safari.name}
-                  </Link>
-                </h3>
-                <p className="text-sm text-gray-500">{safari.description}</p>
-                <div className="flex flex-1 flex-col justify-end">
-                  <p className="text-sm italic text-gray-500">
-                    {safari.status}
-                  </p>
-                  <p className="text-base font-medium text-gray-900">
-                    {safari.price}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+      <div>
+        <GladiolusSafaris />
       </div>
     </>
   );
