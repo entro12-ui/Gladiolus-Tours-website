216d98560f7bf498e54820298604f657d4bc7126
colspan <collin@collindigm.com>
2023-12-14 16:30:56 +0300

    Frontend CI Script: Lint, Build and Test

--- scripts/ci/frontend-format-lint-build-unit-test.sh
+++ scripts/ci/frontend-format-lint-build-unit-test.sh
@@ -9,3 +9,33 @@ if [ $exitStatus -ne 0 ]; then
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     exit $exitStatus
 fi
+
+
+npx nx affected --target=lint --parallel --max-warnings=0
+exitStatus=$?
+if [ $exitStatus -ne 0 ]; then
+    echo -e "\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "One or more lint violations found; fix to continue."
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit $exitStatus
+fi
+
+
+npx nx affected --target=build --prod --parallel
+exitStatus=$?
+if [ $exitStatus -ne 0 ]; then
+    echo -e "\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "One or more modules failed to build; fix to continue."
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit $exitStatus
+fi
+
+
+npx nx affected --target=test --parallel
+exitStatus=$?
+if [ $exitStatus -ne 0 ]; then
+    echo -e "\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "One or more module unit tests failed; fix to continue."
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    exit $exitStatus
+fi
