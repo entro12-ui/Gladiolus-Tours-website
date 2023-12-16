160b0dffdb36e2d9525361e54df9c439cbf3963d
colspan <collin@collindigm.com>
2023-12-16 13:38:18 +0300

    Gladiolus Tours UI: Clean-up

--- apps/ui-gladiolus-tours/src/app/app.tsx
+++ apps/ui-gladiolus-tours/src/app/app.tsx
@@ -1,14 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
 
-import { UiAi } from '@collo/ui-ai';
-
-export function App() {
+export const App = () => {
   return (
     <div>
-      <UiAi/>
+      <h1>On Development</h1>
     </div>
   );
 }
-
-export default App;
