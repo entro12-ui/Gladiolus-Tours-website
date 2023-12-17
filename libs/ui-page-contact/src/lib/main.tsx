c4d8de8fdd1e6667c6ec80713a0300186e8a2b4b
colspan <collin@collindigm.com>
2023-12-17 10:58:07 +0300

    Phone Number Validation

--- libs/ui-page-contact/src/lib/main.tsx
+++ libs/ui-page-contact/src/lib/main.tsx
@@ -262,7 +262,7 @@ export const GladiolusToursContactBody = () => {
                   htmlFor="phone-number"
                   className="block text-sm font-semibold leading-6 text-gray-900"
                 >
-                  Phone number
+                  Phone number *
                 </label>
                 <div className="mt-2.5">
                   <input
@@ -271,8 +271,34 @@ export const GladiolusToursContactBody = () => {
                     id="phone-number"
                     placeholder="Phone Number"
                     autoComplete="tel"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className={classNames(
+                      'block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+                      errors.email?.message
+                        ? 'border-red-300 text-red-900 focus:border-red-500 focus:ring-red-500'
+                        : ''
+                    )}
+                    required
+                    {...register('phoneNumber', {
+                      required: 'Phone Number is required.',
+                      minLength: {
+                        value: 10,
+                        message: 'Minimum length is 10.',
+                      },
+                      maxLength: {
+                        value: 15,
+                        message: 'Maximum length is 15',
+                      },
+                      pattern: {
+                        value: /^[0-9]*$/,
+                        message: 'Only Numbers are allowed',
+                      },
+                    })}
                   />
+                  {errors?.phoneNumber?.message && (
+                    <p className="text-red-600">
+                      {errors.phoneNumber?.message}
+                    </p>
+                  )}
                 </div>
               </div>
               <div className="sm:col-span-2">
