aa7039103117a4958d2f92989923781369c6d113
colspan <collin@collindigm.com>
2024-01-02 13:54:27 +0300

    Clean-up the footer

--- to-frontend/libs/ui-layout-gladiolus/src/lib/footer.tsx
+++ to-frontend/libs/ui-layout-gladiolus/src/lib/footer.tsx
@@ -61,6 +61,7 @@ const navigation = {
 };
 
 export const GladiolusFooter = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-white">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
@@ -92,8 +93,7 @@ export const GladiolusFooter = () => {
           ))}
         </div>
         <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; {new Date().getFullYear()} Gladiolus Tours, Inc. All rights
-          reserved.
+          &copy; {currentYear} Gladiolus Tours, Inc. All rights reserved.
         </p>
       </div>
     </footer>
