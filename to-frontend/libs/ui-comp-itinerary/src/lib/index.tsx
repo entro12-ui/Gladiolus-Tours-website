a8632d1963263966c653ba1b76dbad122659e267
colspan <collin@collindigm.com>
2024-01-06 16:39:25 +0300

    Itinerary List Header add

--- to-frontend/libs/ui-comp-itinerary/src/lib/index.tsx
+++ to-frontend/libs/ui-comp-itinerary/src/lib/index.tsx
@@ -8,13 +8,16 @@ interface IProps {
 export const Itinerary = ({ itinerary }: IProps) => {
   return (
     <section className="bg-gray-100 dark:bg-gray-900 py-10 px-12">
+      <h2 className="text-3xl font-extrabold mb-8 text-gray-800 dark:text-gray-300">
+        Gladiolus Tour Operator Itineraries
+      </h2>
       <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {itinerary.map((itinerary, index) => (
           <div
             key={index}
             className="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1"
           >
-            <Link to="link" className="cursor-pointer">
+            <Link to={`/itinerary/${index}`} className="cursor-pointer">
               <figure>
                 <img
                   src={`${itinerary.image}?auto=format&fit=crop&w=400&q=50`}
