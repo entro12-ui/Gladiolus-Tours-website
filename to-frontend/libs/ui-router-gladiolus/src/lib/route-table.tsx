bb1d96c0b28f8119b775fe66d2f596c7ce593a7e
colspan <collin@collindigm.com>
2024-01-08 11:31:24 +0300

    Route Table Updated

--- to-frontend/libs/ui-router-gladiolus/src/lib/route-table.tsx
+++ to-frontend/libs/ui-router-gladiolus/src/lib/route-table.tsx
@@ -62,7 +62,7 @@ export const GLADIOLUS_TOURS_ROUTE_TABLE = [
         element: <LazyDestinationsPage />,
       },
       {
-        path: GtToursRoute.itineraries,
+        path: GtToursRoute.Itineraries,
         index: true,
         element: <LazyItinerariesPage />,
       },
