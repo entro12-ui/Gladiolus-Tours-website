c033042c4bc971254c343f8267c7b5aa0abf54b3
colspan <collin@collindigm.com>
2024-01-18 14:44:33 +0300

    Update the title of the header

--- to-frontend/libs/ui-page-itinerary/src/lib/accordion-item.tsx
+++ to-frontend/libs/ui-page-itinerary/src/lib/accordion-item.tsx
@@ -14,7 +14,9 @@ export const AccordionItem = ({ title, children }: AccordionItemProps) => {
         className="flex items-center justify-between cursor-pointer p-4 bg-white dark:bg-gray-800 rounded-md"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <h2 className="text-xl font-bold">{title}</h2>
+        <h3 className="text-2xl font-semibold mb-2 text-indigo-600 dark:text-indigo-400">
+          {title}
+        </h3>
         <div>{isOpen ? '▼' : '►'}</div>
       </div>
       {isOpen && (
