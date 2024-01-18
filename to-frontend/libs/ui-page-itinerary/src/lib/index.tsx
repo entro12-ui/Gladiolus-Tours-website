43ec3e079db39846eb9901772f44690d52fe2f9e
colspan <collin@collindigm.com>
2024-01-18 14:45:07 +0300

    Slider added

--- to-frontend/libs/ui-page-itinerary/src/lib/index.tsx
+++ to-frontend/libs/ui-page-itinerary/src/lib/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GtItinerary } from '@collo/ui-persistance';
 import {
   GtToursRoute,
@@ -6,6 +6,60 @@ import {
 } from '@collo/ui-routes-gladiolus';
 import { generatePath, Navigate } from 'react-router-dom';
 import { AccordionItem } from './accordion-item';
+import './ImageSlider.css';
+
+interface ImageSliderProps {
+  images: string[];
+}
+
+const ImageSlider = ({ images }: ImageSliderProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setCurrentIndex(0);
+    };
+
+    const preloadImages = () => {
+      images.forEach((image) => {
+        const img = new Image();
+        img.src = image;
+        img.onload = handleLoad;
+      });
+    };
+
+    preloadImages();
+  }, [images]);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div className="slider">
+      <button className="arrow" onClick={prevSlide}>
+        {'<'}
+      </button>
+      <div className="slide-container">
+        <div
+          className="slide"
+          style={{
+            backgroundImage: `url(${images[currentIndex]})`,
+          }}
+        ></div>
+      </div>
+      <button className="arrow" onClick={nextSlide}>
+        {'>'}
+      </button>
+    </div>
+  );
+};
 
 export const GladiolusToursItineraryDetails = () => {
   const itineraryId = useItineraryIdOrThrow();
@@ -15,25 +69,27 @@ export const GladiolusToursItineraryDetails = () => {
     return <Navigate to={generatePath(GtToursRoute.NotFound)} />;
   }
 
+  const carouselImages = itinerary.carouselImage.map((step) => step);
+
   return (
-    <div className="container mx-auto py-10">
-      <div className="space-y-8">
-        {itinerary.description.map((step, index) => (
-          <AccordionItem key={index} title={step.title}>
-            <div className="mb-6">
-              <h3 className="text-2xl font-semibold mb-2 text-indigo-600 dark:text-indigo-400">
-                {step.title}
-              </h3>
-              <p className="mb-2 text-gray-700 dark:text-gray-300">
-                {step.Accommodation && `Accommodation: ${step.Accommodation}`}
-              </p>
-              <p className="text-gray-800 dark:text-gray-400">
-                {step.description}
-              </p>
-            </div>
-          </AccordionItem>
-        ))}
+    <>
+      <ImageSlider images={carouselImages} />
+      <div className="container mx-auto py-10">
+        <div className="space-y-8">
+          {itinerary.description.map((step, index) => (
+            <AccordionItem key={index} title={step.title}>
+              <div className="mb-6">
+                <p className="mb-2 text-gray-700 dark:text-gray-300">
+                  {step.Accommodation && `Accommodation: ${step.Accommodation}`}
+                </p>
+                <p className="text-gray-800 dark:text-gray-400">
+                  {step.description}
+                </p>
+              </div>
+            </AccordionItem>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
