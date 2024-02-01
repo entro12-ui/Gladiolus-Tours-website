f5f60bf0f74ec937fea2b9d8911be301ea70f024
colspan <collin@collindigm.com>
2024-02-02 01:12:18 +0300

    Fix the Link to the next page

--- to-frontend/libs/ui-page-destinations/src/lib/main.tsx
+++ to-frontend/libs/ui-page-destinations/src/lib/main.tsx
@@ -1,4 +1,6 @@
 import { GtDestinations } from '@collo/ui-persistance';
+import { generatePath, Link } from 'react-router-dom';
+import { GtToursRoute } from '@collo/ui-routes-gladiolus';
 
 export const GladiolusToursDestinations = () => {
   return (
@@ -7,7 +9,7 @@ export const GladiolusToursDestinations = () => {
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-          {GtDestinations.map((destination) => (
+          {GtDestinations.map((destination, index) => (
             <div
               key={destination.id}
               className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
@@ -21,10 +23,12 @@ export const GladiolusToursDestinations = () => {
               </div>
               <div className="flex flex-1 flex-col space-y-2 p-4">
                 <h3 className="text-sm font-medium text-gray-900">
-                  <a href={destination.href}>
-                    <span aria-hidden="true" className="absolute inset-0" />
-                    {destination.name}
-                  </a>
+                  <Link
+                    to={generatePath(GtToursRoute.DestinationDetails, {
+                      destinationId: String(index),
+                    })}
+                    className="block"
+                  />
                 </h3>
                 <p className="text-sm text-gray-500">
                   {destination.description}
