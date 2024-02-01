1b40609428f2b9f3969c89b21bb205417a188bff
colspan <collin@collindigm.com>
2024-02-01 13:00:51 +0300

    Gladiolus Tours Adventures

--- to-frontend/libs/ui-page-adventures/src/lib/index.tsx
+++ to-frontend/libs/ui-page-adventures/src/lib/index.tsx
@@ -1,7 +1,47 @@
+import { generatePath, Link } from 'react-router-dom';
+import { GtToursRoute } from '@collo/ui-routes-gladiolus';
+import { GtAdventures, GtItineraryCategory } from '@collo/ui-persistance';
+
 export const GladiolusToursAdventures = () => {
   return (
-    <div>
-      <h1>Gladiolus Tours Adventures</h1>
+    <div className="container mx-auto py-10">
+      <h2 className="text-4xl font-extrabold mb-8 text-gray-800 dark:text-gray-300 text-center">
+        Discover Your Next Adventure with Gladiolus Tours
+      </h2>
+      <div className="grid grid-flow-row gap-8 justify-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+        {GtAdventures.map((item, index) => (
+          <div
+            key={index}
+            className="my-8 rounded-lg overflow-hidden shadow-lg bg-white dark:bg-gray-800 duration-300 hover:transform hover:scale-105"
+          >
+            <Link
+              to={generatePath(GtToursRoute.AdventuresDetails, {
+                adventuresId: String(index),
+              })}
+              className="block"
+            >
+              <figure>
+                <img
+                  src={`${item.image}?auto=format&fit=crop&w=400&q=50`}
+                  alt={item.title}
+                  className="rounded-t h-64 w-full object-cover"
+                />
+                <figcaption className="p-4">
+                  <div className="mb-3 text-sm text-gray-500 dark:text-gray-400">
+                    Category: {GtItineraryCategory[item.category]}
+                  </div>
+                  <p className="text-xl mb-3 font-semibold leading-tight text-gray-800 dark:text-gray-300">
+                    {item.title}
+                  </p>
+                  <p className="text-sm leading-5 text-gray-600 dark:text-gray-400">
+                    {item.overview}
+                  </p>
+                </figcaption>
+              </figure>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
