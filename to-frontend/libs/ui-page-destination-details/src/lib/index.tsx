987bccc621ba92b522284e2d8d57914aa0cf2fcf
colspan <collin@collindigm.com>
2024-02-14 11:20:40 +0300

    Fetch a single Destination from the list

--- to-frontend/libs/ui-page-destination-details/src/lib/index.tsx
+++ to-frontend/libs/ui-page-destination-details/src/lib/index.tsx
@@ -1,7 +1,22 @@
+import {
+  GtToursRoute,
+  useDestinationIdOrThrow,
+} from '@collo/ui-routes-gladiolus';
+import { GtDestinations } from '@collo/ui-persistance';
+import { Navigate } from 'react-router-dom';
+
 export const GladiolusToursDestinationsDetails = () => {
+  const destinationId = useDestinationIdOrThrow();
+  const destination = GtDestinations[destinationId];
+
+  if (!destination) {
+    return <Navigate to={GtToursRoute.NotFound} />;
+  }
+
   return (
-    <div>
-      <h1>This is a destination Details Page</h1>
+    <div className="destinations-container">
+      <h1>Gladiolus Tours Destinations</h1>
+      <p>Discover exciting adventures in Tanzania:</p>
     </div>
   );
 };
