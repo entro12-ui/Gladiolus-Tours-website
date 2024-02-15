e68f7c5336e9a7e0f512eacac037f95f62139f58
colspan <collin@collindigm.com>
2024-02-15 22:35:40 +0300

    Filter Initiated to the Destination

--- to-frontend/libs/ui-page-destinations/src/lib/main.tsx
+++ to-frontend/libs/ui-page-destinations/src/lib/main.tsx
@@ -3,11 +3,13 @@ import { GtDestinationCategory, GtDestinations } from '@collo/ui-persistance';
 import { generatePath, Link } from 'react-router-dom';
 import { GtToursRoute } from '@collo/ui-routes-gladiolus';
 import { UiStatus } from '@collo/ui-comp-status';
+import { classNames } from '@collo/ui-utils';
 
 export const GladiolusToursDestinations = () => {
   // Use state with type for selectedCircuit
-  const [selectedCircuit, setSelectedCircuit] =
-    useState<GtDestinationCategory>();
+  const [selectedCircuit, setSelectedCircuit] = useState<GtDestinationCategory>(
+    GtDestinationCategory['Northern Circuit']
+  );
 
   // Define filter options with type
   const filterOptions: {
@@ -62,22 +64,10 @@ export const GladiolusToursDestinations = () => {
             >
               Filter by Circuit:
             </label>
-            <select
-              id="filter-circuit"
-              value={selectedCircuit}
-              onChange={(e) =>
-                setSelectedCircuit(
-                  e.target.value as unknown as GtDestinationCategory
-                )
-              }
-              className="rounded-md border border-gray-300 px-3 py-2 text-gray-700 shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-            >
-              {filterOptions.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
+            <Tab
+              selectedCategory={selectedCircuit}
+              onTabChange={setSelectedCircuit}
+            />
           </div>
         </div>
       </div>
@@ -117,6 +107,94 @@ export const GladiolusToursDestinations = () => {
             </Link>
           ))}
         </div>
+      </div>
+    </div>
+  );
+};
+
+interface TabProps {
+  selectedCategory: GtDestinationCategory;
+  onTabChange: (category: GtDestinationCategory) => void;
+}
+
+const Tab = ({ selectedCategory, onTabChange }: TabProps) => {
+  const tabs = [
+    {
+      name: 'Northern Circuit',
+      category: GtDestinationCategory['Northern Circuit'],
+    },
+    {
+      name: 'Southern Circuit',
+      category: GtDestinationCategory['Southern Circuit'],
+    },
+    {
+      name: 'Eastern Circuit',
+      category: GtDestinationCategory['Eastern Circuit'],
+    },
+    {
+      name: 'Western Circuit',
+      category: GtDestinationCategory['Western Circuit'],
+    },
+    {
+      name: 'Ocean Islands',
+      category: GtDestinationCategory['Ocean Islands'],
+    },
+    {
+      name: 'Mafia Island',
+      category: GtDestinationCategory['Mafia Island'],
+    },
+    {
+      name: 'Zanzibar Island',
+      category: GtDestinationCategory['Zanzibar Island'],
+    },
+  ];
+
+  return (
+    <div>
+      <div className="sm:hidden">
+        <label htmlFor="tabs" className="sr-only">
+          Filter
+        </label>
+        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+        <select
+          id="tabs"
+          name="tabs"
+          className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+          value={selectedCategory}
+          onChange={(e) => onTabChange(parseInt(e.target.value))}
+        >
+          {tabs.map((tab) => (
+            <option key={tab.category} value={tab.category}>
+              {tab.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="hidden sm:block">
+        <nav
+          className="isolate flex divide-x divide-gray-200 rounded-lg shadow"
+          aria-label="Tabs"
+        >
+          {tabs.map((tab, tabIdx) => (
+            <button
+              key={tab.category}
+              onClick={() => onTabChange(tab.category)}
+              className={classNames(
+                tab.category === selectedCategory
+                  ? 'text-gray-900'
+                  : 'text-gray-500 hover:text-gray-700',
+                tabIdx === 0 ? 'rounded-l-lg' : '',
+                tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
+                'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10'
+              )}
+              aria-current={
+                tab.category === selectedCategory ? 'page' : undefined
+              }
+            >
+              {tab.name}
+            </button>
+          ))}
+        </nav>
       </div>
     </div>
   );
