3381e362c0ead68cba158b9d39dc3a0efde2457d
colspan <collin@collindigm.com>
2024-02-22 13:45:07 +0300

    Updated the Destination List display to display each national Park individual

--- to-frontend/libs/ui-page-destinations/src/lib/destination-list.tsx
+++ to-frontend/libs/ui-page-destinations/src/lib/destination-list.tsx
@@ -3,7 +3,6 @@ import {
   GtDestinationCategory,
   GladiolusToursDestinations,
   GtNationalParks,
-  GtItineraryCategory,
 } from '@collo/ui-persistance';
 import { generatePath, Link } from 'react-router-dom';
 import { GtToursRoute } from '@collo/ui-routes-gladiolus';
@@ -23,43 +22,38 @@ export const DestinationList = ({
       : destinations.filter((destination) => destination.category === filter);
 
   return (
-    <div className="grid grid-flow-row gap-8 justify-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-      {filteredDestinations.map((destination, index) => (
-        <div
-          key={index}
-          className="my-8 rounded-lg overflow-hidden shadow-lg bg-white dark:bg-gray-800 duration-300 hover:transform hover:scale-105"
-        >
-          <Link
-            to={generatePath(GtToursRoute.DestinationDetails, {
-              destinationId: String(index),
-            })}
-            className="block"
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      {filteredDestinations.flatMap((destination) =>
+        destination.nationalParks.map((park: GtNationalParks, index) => (
+          <div
+            key={index}
+            className="rounded-lg overflow-hidden shadow-lg bg-white dark:bg-gray-800 duration-300 transform hover:scale-105"
           >
-            <figure key={destination.id}>
-              {destination.nationalParks.map((park: GtNationalParks) => (
-                <React.Fragment key={destination.id}>
-                  <img
-                    src={`${park.imageSrc}?auto=format&fit=crop&w=400&q=50`}
-                    alt={park.imageAlt}
-                    className="rounded-t h-64 w-full object-cover"
-                  />
-                  <figcaption className="p-4">
-                    <div className="mb-3 text-sm text-gray-500 dark:text-gray-400">
-                      Category: {GtItineraryCategory[park.category]}
-                    </div>
-                    <p className="text-xl mb-3 font-semibold leading-tight text-gray-800 dark:text-gray-300">
-                      {park.name}
-                    </p>
-                    <p className="text-sm leading-5 text-gray-600 dark:text-gray-400">
-                      {park.description}
-                    </p>
-                  </figcaption>
-                </React.Fragment>
-              ))}
-            </figure>
-          </Link>
-        </div>
-      ))}
+            <Link
+              to={generatePath(GtToursRoute.DestinationDetails, {
+                destinationId: String(destination.id),
+              })}
+              className="block"
+            >
+              <figure>
+                <img
+                  src={`${park.imageSrc}?auto=format&fit=crop&w=400&q=50`}
+                  alt={park.imageAlt}
+                  className="h-64 w-full object-cover rounded-t"
+                />
+                <figcaption className="p-4">
+                  <p className="text-xl mb-3 font-semibold leading-tight text-gray-800 dark:text-gray-300">
+                    {park.name}
+                  </p>
+                  <p className="text-sm leading-5 text-gray-600 dark:text-gray-400">
+                    {park.description}
+                  </p>
+                </figcaption>
+              </figure>
+            </Link>
+          </div>
+        ))
+      )}
     </div>
   );
 };
