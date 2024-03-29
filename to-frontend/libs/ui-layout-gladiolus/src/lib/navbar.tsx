79e2948c8c821663f0fe54ee8cbf5825e2a89385
colspan <collin@collindigm.com>
2024-03-29 07:21:25 +0300

    Close Mobile Menu when navbar is clicked

--- to-frontend/libs/ui-layout-gladiolus/src/lib/navbar.tsx
+++ to-frontend/libs/ui-layout-gladiolus/src/lib/navbar.tsx
@@ -188,7 +188,6 @@ export const GladiolusHeader = () => {
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
@@ -202,6 +201,7 @@ export const GladiolusHeader = () => {
                     key={item.name}
                     to={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </Link>
