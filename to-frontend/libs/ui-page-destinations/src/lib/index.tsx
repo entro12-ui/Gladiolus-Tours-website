99924296e9f4f413bbe7a90c4eccdb15bf725ac3
colspan <collin@collindigm.com>
2024-04-01 09:03:33 +0300

    Page clean-up

--- to-frontend/libs/ui-page-destinations/src/lib/index.tsx
+++ to-frontend/libs/ui-page-destinations/src/lib/index.tsx
@@ -1,35 +1,11 @@
 import { EntryPageHelmet } from '@collo/ui-comp-page-helmet';
-import { DestinationFilter } from './destination-filter';
-import { DestinationList } from './destination-list';
-import { useState } from 'react';
-import { GtDestinationCategory, GtDestinations } from '@collo/ui-persistance';
+import { Destinations } from './destinations';
 
 export const GladiolusDestinationsPage = () => {
-  const [destinations] = useState(GtDestinations);
-  const [filter, setFilter] = useState<GtDestinationCategory>(
-    GtDestinationCategory['All Circuits']
-  );
-
-  const handleFilterChange = (newFilter: GtDestinationCategory) => {
-    setFilter(newFilter);
-  };
-
   return (
     <div className="bg-white">
       <EntryPageHelmet title={'Safari Destinations'} />
-      {/*
-      <GladiolusHeaderSection
-        header={'Safari Destinations'}
-        description={'Explore Gladiolus Tours Destinations'}
-      />
-      */}
-      <div>
-        <DestinationFilter
-          selectedFilter={filter}
-          onFilterChange={handleFilterChange}
-        />
-        <DestinationList filter={filter} destinations={destinations} />
-      </div>
+      <Destinations />
     </div>
   );
 };
