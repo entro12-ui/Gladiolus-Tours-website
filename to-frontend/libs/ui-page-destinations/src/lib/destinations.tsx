b1cbe576fad1f80a7301fa240432fe14dd8cacc0
colspan <collin@collindigm.com>
2024-04-03 09:28:09 +0300

    Products Grid clean-up to use GT Destinations

--- to-frontend/libs/ui-page-destinations/src/lib/destinations.tsx
+++ to-frontend/libs/ui-page-destinations/src/lib/destinations.tsx
@@ -9,6 +9,8 @@ import {
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import { classNames } from '@collo/ui-utils';
+import { GtDestinations } from '@collo/ui-persistance';
+import { Link } from '@mui/material';
 
 const sortOptions = [
   { name: 'Most Popular', href: '#' },
@@ -64,41 +66,6 @@ const filters = [
       { value: 'xl', label: 'XL' },
       { value: '2xl', label: '2XL' },
     ],
-  },
-];
-
-const products = [
-  {
-    id: 1,
-    name: 'Focus Paper Refill',
-    href: '#',
-    price: '$13',
-    description: '3 sizes available',
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-01.jpg',
-    imageAlt:
-      'Person using a pen to cross a task off a productivity paper card.',
-  },
-  {
-    id: 2,
-    name: 'Focus Card Holder',
-    href: '#',
-    price: '$64',
-    description: 'Walnut',
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-02.jpg',
-    imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
-  },
-  {
-    id: 3,
-    name: 'Focus Carry Pouch',
-    href: '#',
-    price: '$32',
-    description: 'Heather Gray',
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-03.jpg',
-    imageAlt:
-      'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
   },
 ];
 
@@ -360,24 +327,26 @@ export function Destinations() {
               </h2>
 
               <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-                {products.map((product) => (
-                  <a key={product.id} href={product.href} className="group">
-                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg sm:aspect-h-3 sm:aspect-w-2">
-                      <img
-                        src={product.imageSrc}
-                        alt={product.imageAlt}
-                        className="h-full w-full object-cover object-center group-hover:opacity-75"
-                      />
-                    </div>
-                    <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                      <h3>{product.name}</h3>
-                      <p>{product.price}</p>
-                    </div>
-                    <p className="mt-1 text-sm italic text-gray-500">
-                      {product.description}
-                    </p>
-                  </a>
-                ))}
+                {GtDestinations.map((destination) =>
+                  destination.nationalParks.map((parks) => (
+                    <Link key={parks.id} href={parks.name} className="group">
+                      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg sm:aspect-h-3 sm:aspect-w-2">
+                        <img
+                          src={parks.imageSrc}
+                          alt={parks.imageAlt}
+                          className="h-full w-full object-cover object-center group-hover:opacity-75"
+                        />
+                      </div>
+                      <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
+                        <h3>{parks.name}</h3>
+                        <p>{parks.rate}</p>
+                      </div>
+                      <p className="mt-1 text-sm italic text-gray-500">
+                        {parks.description}
+                      </p>
+                    </Link>
+                  ))
+                )}
               </div>
             </section>
           </div>
