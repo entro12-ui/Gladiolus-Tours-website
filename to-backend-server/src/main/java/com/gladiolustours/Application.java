fad8555377700a5538fba769ef2238d45f6526fe
colspan <collin@collindigm.com>
2024-04-11 09:45:33 +0300

    Backend Main class updated

--- to-backend-server/src/main/java/com/gladiolustours/Application.java
+++ to-backend-server/src/main/java/com/gladiolustours/Application.java
@@ -1,11 +1,11 @@
-package com.collo;
+package com.gladiolustours;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
