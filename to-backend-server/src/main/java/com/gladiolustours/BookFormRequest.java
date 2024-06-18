9aed325b75480a19b5849632bb93549f177263a3
colspan <collin@collindigm.com>
2024-06-18 23:01:12 +0300

    Safari Book Form Request modal

---
+++ to-backend-server/src/main/java/com/gladiolustours/BookFormRequest.java
@@ -0,0 +1,11 @@
+public class BookFormRequest {
+
+    private String fullName;
+    private String email;
+    private String dateFrom;
+    private String dateTo;
+    private Integer adults;
+    private Integer child;      // 0 - 5
+    private String moreInfo;
+
+}
