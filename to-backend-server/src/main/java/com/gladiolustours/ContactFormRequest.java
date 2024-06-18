9aef388faa9e5dc5a58dcdd00ca852ffe08a856d
colspan <collin@collindigm.com>
2024-06-18 15:49:55 +0300

    Contact Form modal initialized

---
+++ to-backend-server/src/main/java/com/gladiolustours/ContactFormRequest.java
@@ -0,0 +1,52 @@
+package com.gladiolustours;
+
+public class ContactFormRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String message;
+
+    public ContactFormRequest() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
