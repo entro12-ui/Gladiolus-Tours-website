3567848c68afdf52de1eb0dbfc1edc44a72355c9
colspan <collin@collindigm.com>
2024-06-19 18:14:38 +0300

    Booking: Fields and methods defined

--- to-backend-server/src/main/java/com/gladiolustours/model/Booking.java
+++ to-backend-server/src/main/java/com/gladiolustours/model/Booking.java
@@ -1,6 +1,6 @@
 package com.gladiolustours.model;
 
-public class BookFormRequest {
+public class Booking {
 
     private String fullName;
     private String email;
@@ -9,5 +9,80 @@ public class BookFormRequest {
     private Integer adults;
     private Integer child;      // 0 - 5
     private String moreInfo;
+    private String code;
 
+    public Booking(String fullName, String email, String dateFrom, String dateTo, Integer adults, Integer child, String moreInfo, String code) {
+        this.fullName = fullName;
+        this.email = email;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+        this.adults = adults;
+        this.child = child;
+        this.moreInfo = moreInfo;
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateFrom() {
+        return dateFrom;
+    }
+
+    public void setDateFrom(String dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+
+    public String getDateTo() {
+        return dateTo;
+    }
+
+    public void setDateTo(String dateTo) {
+        this.dateTo = dateTo;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMoreInfo(String moreInfo) {
+        this.moreInfo = moreInfo;
+    }
+
+    public String getMoreInfo() {
+        return moreInfo;
+    }
+
+    public void setChild(Integer child) {
+        this.child = child;
+    }
+
+    public Integer getChild() {
+        return child;
+    }
+
+    public void setAdults(Integer adults) {
+        this.adults = adults;
+    }
+
+    public Integer getAdults() {
+        return adults;
+    }
 }
