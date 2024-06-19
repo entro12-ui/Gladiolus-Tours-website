3879053d73ab68ec178560b287b1806be8edbad2
colspan <collin@collindigm.com>
2024-06-20 01:20:04 +0300

    Shell Script to test the endpoint requests

---
+++ request.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+curl -X POST http://localhost:8080/createBooking -H "Content-Type: application/json" -d '{
+  "fullName": "John Doe",
+  "email": "johndoe@example.com",
+  "subject": "Booking Inquiry",
+  "dateFrom": "20/07/2024",
+  "dateTo": "25/07/2024",
+  "adults": 2,
+  "child": 2,
+  "moreInfo": "I would like to book a tour.",
+  "code": "GT001"
+}'
+
+curl -X POST http://localhost:8080/createContact -H "Content-Type: application/json" -d '{
+  "firstName": "Jane",
+  "lastName": "Doe",
+  "subject": "General Inquiry",
+  "email": "janedoe@example.com",
+  "phoneNumber": "+255-762-578-062",
+  "message": "I have a question about your tours."
+}'
+
