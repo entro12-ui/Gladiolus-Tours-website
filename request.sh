ed5022a8e2909e8fea812cf6a66a1190369c2858
colspan <collin@collindigm.com>
2024-06-20 15:11:32 +0300

    Update the email sender for testing

--- request.sh
+++ request.sh
@@ -2,7 +2,7 @@
 
 curl -X POST http://localhost:8080/createBooking -H "Content-Type: application/json" -d '{
   "fullName": "John Doe",
-  "email": "johndoe@example.com",
+  "email": "collo@fastmail.com",
   "subject": "Booking Inquiry",
   "dateFrom": "20/07/2024",
   "dateTo": "25/07/2024",
@@ -16,7 +16,7 @@ curl -X POST http://localhost:8080/createContact -H "Content-Type: application/j
   "firstName": "Jane",
   "lastName": "Doe",
   "subject": "General Inquiry",
-  "email": "janedoe@example.com",
+  "email": "collo@fastmail.com",
   "phoneNumber": "+255-762-578-062",
   "message": "I have a question about your tours."
 }'
