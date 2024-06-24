9effa1bbacaec8f8afa5be2ff628a4eb90b4b4be
colspan <collin@collindigm.com>
2024-06-24 11:46:20 +0300

    Controller Layer: Contact endpoint initialized for the contact

---
+++ to-backend-server/src/main/java/com/gladiolustours/controller/ContactController.java
@@ -0,0 +1,27 @@
+package com.gladiolustours.controller;
+
+import com.gladiolustours.model.Contact;
+import com.gladiolustours.service.EmailService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contact")
+public class ContactController {
+
+    private final EmailService emailService;
+
+    public ContactController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
+        emailService.send(contact);
+        return new ResponseEntity<>(contact, HttpStatus.CREATED);
+    }
+}
