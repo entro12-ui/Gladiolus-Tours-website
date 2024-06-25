a52ada7d53219ee8fdf63706e4898001ba750183
colspan <collin@collindigm.com>
2024-06-25 11:43:17 +0300

    Controller Layer: Booking Controller initialized to POST a booking

---
+++ to-backend-server/src/main/java/com/gladiolustours/controller/BookingController.java
@@ -0,0 +1,29 @@
+package com.gladiolustours.controller;
+
+import com.gladiolustours.model.Booking;
+import com.gladiolustours.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/booking")
+public class BookingController {
+
+    private final EmailService emailService;
+
+    @Autowired
+    public BookingController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
+        emailService.send(booking);
+        return new ResponseEntity<>(booking, HttpStatus.CREATED);
+    }
+}
