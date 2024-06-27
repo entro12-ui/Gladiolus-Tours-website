4d1e900d2b9737f59b38ae299b2b75b9a414db57
colspan <collin@collindigm.com>
2024-06-27 11:28:34 +0300

    Contact Mutation Updated

--- to-frontend/libs/ui-api/src/lib/mutations/mutations.tsx
+++ to-frontend/libs/ui-api/src/lib/mutations/mutations.tsx
@@ -5,7 +5,7 @@ import { GtNotify } from '@collo/ui-comp-notifications';
 /**
  * Contact Form
  */
-export type GladiolusToursRequest = {
+export type GladiolusToursContactRequest = {
   firstName: string;
   lastName: string;
   subject: string;
@@ -14,28 +14,39 @@ export type GladiolusToursRequest = {
   message: string;
 };
 
-export type GladiolusToursResponse = {
-  firstName: string;
-  lastName: string;
-  subject: string;
-  email: string;
-  phoneNumber: string;
-  message: string;
+export type GladiolusToursContactResponse = {
+  data: string;
+  status: number;
 };
 
-export const useGladiolusToursContactMutation = (
-  onSuccess: (response: GladiolusToursResponse) => void
-) => {
-  return useMutation<GladiolusToursResponse, Error, GladiolusToursRequest>(
+export const useGladiolusToursContactMutation = () => {
+  return useMutation<
+    GladiolusToursContactResponse,
+    Error,
+    GladiolusToursContactRequest
+  >(
     async (data) => {
       return await apiClient.post(`/contact`, data);
     },
     {
-      onSuccess: (success) => {
-        GtNotify.success('Message Sent', `${success.message}`);
+      onSuccess: (response) => {
+        if (response.status === 201) {
+          GtNotify.success(
+            'Message Sent',
+            `Your message has been successfully sent.`
+          );
+        } else {
+          GtNotify.info(
+            'Message Sent',
+            `Your message has been sent, but there was an unexpected status code.`
+          );
+        }
       },
-      onError: (error) => {
-        GtNotify.danger('Failed To Send Message', `${error.message}`);
+      onError: () => {
+        GtNotify.danger(
+          'Server Error',
+          `There was a server error. Please try again later.`
+        );
       },
     }
   );
