60b16b7ca1bae3faf75cc854103de8b83d6326e2
colspan <collin@collindigm.com>
2024-06-27 10:43:02 +0300

    Client base URL updated to prod

--- to-frontend/libs/ui-api/src/lib/client.ts
+++ to-frontend/libs/ui-api/src/lib/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://p6v8eafh3f.execute-api.af-south-1.amazonaws.com/prev';
+const baseURL = 'https://p6v8eafh3f.execute-api.af-south-1.amazonaws.com/prod';
 
 export const apiClient = axios.create({
   baseURL,
