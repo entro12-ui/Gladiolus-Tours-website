56c3f104348b449450df6435c995345fe934ff2b
colspan <collin@collindigm.com>
2024-07-02 10:42:40 +0300

    Updated the email and format the response with jq

--- request.sh
+++ request.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-curl -X POST http://localhost:8080/booking -H "Content-Type: application/json" -d '{
+# Booking Inquiry
+response_booking=$(curl -s -X POST http://localhost:8080/booking -H "Content-Type: application/json" -d '{
   "fullName": "John Doe",
-  "email": "collo@fastmail.com",
+  "email": "info@gladiolustours.com",
   "subject": "Booking Inquiry",
   "dateFrom": "20/07/2024",
   "dateTo": "25/07/2024",
@@ -10,14 +11,17 @@ curl -X POST http://localhost:8080/booking -H "Content-Type: application/json" -
   "child": 2,
   "moreInfo": "I would like to book a tour.",
   "code": "GT001"
-}'
+}')
+echo "$response_booking" | jq .
 
-curl -X POST http://localhost:8080/contact -H "Content-Type: application/json" -d '{
+# General Inquiry
+response_contact=$(curl -s -X POST http://localhost:8080/contact -H "Content-Type: application/json" -d '{
   "firstName": "Jane",
   "lastName": "Doe",
   "subject": "General Inquiry",
-  "email": "collo@fastmail.com",
+  "email": "info@gladiolustours.com",
   "phoneNumber": "+255-762-578-062",
   "message": "I have a question about your tours."
-}'
+}')
+echo "$response_contact" | jq .
 
