a671ed5b5d4f649c65bcedeaae34f6234badb0a4
TheCollinsByte <collo@fastmail.com>
2025-11-23 11:09:46 +0300

    icon add for dark and light

--- gt-frontend/app/layout.tsx
+++ gt-frontend/app/layout.tsx
@@ -37,19 +37,19 @@ export const metadata: Metadata = {
         url: "/favicon.ico",
       },
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon-light.svg",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/icon-dark.svg",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/icon-light.svg",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/icon-dark.svg",
   },
   manifest: "/manifest.json",
   openGraph: {
