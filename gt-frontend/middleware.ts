ad82106d60cc6a90814fe29940be1c1f316193b7
TheCollinsByte <collo@fastmail.com>
2025-11-29 01:55:42 +0300

    i18n middleware defined

---
+++ gt-frontend/middleware.ts
@@ -0,0 +1,11 @@
+import createMiddleware from 'next-intl/middleware'
+import { routing } from './i18n/routing'
+
+export default createMiddleware({
+  ...routing,
+  localePrefix: 'always'
+})
+
+export const config = {
+  matcher: ['/', '/((?!_next|_vercel|.*\\..*).*)']
+}
