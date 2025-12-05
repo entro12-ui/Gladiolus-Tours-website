d5433c581649e48c26caf88114ff0a5b26faf64b
TheCollinsByte <collo@fastmail.com>
2025-12-05 13:07:13 +0300

    NextJS Config Updated to allow assets hostname

--- gt-frontend/next.config.mjs
+++ gt-frontend/next.config.mjs
@@ -8,7 +8,13 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.gladiolustours.com',
+        pathname: '/**',
+      },
+    ],
   },
   // Optimize for production
   compress: true,
